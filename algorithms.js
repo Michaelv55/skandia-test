6d6dc56347d65017614e0874d83b45213011d05c
Michael Velasco <maicolvelasco55@gmail.com>
2022-11-10 23:39:23 -0500

    add second test

--- algorithms.js
+++ algorithms.js
@@ -12,3 +12,29 @@ const binGo = (size = 100) => {
     return obj;
   }, {});
 }
+
+/**
+ * Check if a number is prime
+ * @author Michael Velasco
+ * @param {number} num number to validate
+ * @returns {boolean}
+ */
+ const isPrime = num => {
+  const length = Math.sqrt(num);
+  return Array.from({ length }, (_, i) => 2+i).some((val) => {
+    return num % val === 0;
+  }) ? false : (num > 1); 
+}
+
+/**
+ * Second test for Skandia primeNumbers (list of prime numbers)
+ * @author Michael Velasco
+ * @param {number} num number to validate
+ * @returns {boolean}
+ */
+const primeNumbers = (size = 50) => {
+  return [...Array(size).keys()].reduce((obj, val) => {
+    isPrime(val+1) ? obj.push(val+1) : obj;
+    return obj;
+  }, [2]);
+}
